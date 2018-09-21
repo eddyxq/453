861123af0e72bc7501f1f46e1b26c56e845471f1
eddyxq <eddy@ualberta.ca>
2018-09-20 23:11:49 -0600

    updated documentation

--- Assignment1/RefactoredBoilerplate/Scene.cpp
+++ Assignment1/RefactoredBoilerplate/Scene.cpp
@@ -1,14 +1,12 @@
 /*
- * Scene.cpp
- *
- *  Created on: Sep 10, 2018
- *  Author: John Hall
+ * This class contains the scenes
+ * @author Eddy Qiang - CPSC 453 - Tutorial T01
+ * @version 1.2 
+ * @since September 20, 2018
  */
 
 #include "Scene.h"
-
 #include "RenderingEngine.h"
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -30,21 +28,13 @@ void Scene::displayScene() {
 }
 
 void Scene::displaySquaresAndDiamondsScene(int iteration) {
-
 	objects.clear();
-	//Create a single triangle
-	//Additional triangles can be created by pushing groups of three more vertices into the verts vector
 	Geometry square;
-
-	float postiveBorderValue = 0.9f;
-	float negativeBorderValue = -0.9f;
-
-	//float iteration = 6;
 	float scalingFactor = 1;
 	double shadeFactor = 1;
-
+	//Create the squares and diamonds
 	for (int x = 0; x < iteration; x++) {
-		//squares
+		//Squares
 		square.verts.push_back(glm::vec3(-0.9f/scalingFactor, -0.9f/scalingFactor, 1.0f));
 		square.verts.push_back(glm::vec3(0.9f/scalingFactor, -0.9f/scalingFactor, 1.0f));
 		square.verts.push_back(glm::vec3(0.9f/scalingFactor, 0.9f/scalingFactor, 1.0f));
@@ -53,7 +43,7 @@ void Scene::displaySquaresAndDiamondsScene(int iteration) {
 		square.verts.push_back(glm::vec3(-0.9f/scalingFactor, 0.9f/scalingFactor, 1.0f));
 		square.verts.push_back(glm::vec3(0.9f/scalingFactor, 0.9f/scalingFactor, 1.0f));
 		square.verts.push_back(glm::vec3(0.9f/scalingFactor, -0.9f/scalingFactor, 1.0f));
-		//diamonds
+		//Diamonds
 		square.verts.push_back(glm::vec3(-0.9f /scalingFactor, 0.0f /scalingFactor, 1.0f));
 		square.verts.push_back(glm::vec3(0.0f /scalingFactor, -0.9f /scalingFactor, 1.0f));
 		square.verts.push_back(glm::vec3(0.9f /scalingFactor, 0.0f /scalingFactor, 1.0f));
@@ -62,10 +52,10 @@ void Scene::displaySquaresAndDiamondsScene(int iteration) {
 		square.verts.push_back(glm::vec3(0.0f /scalingFactor, 0.9f /scalingFactor, 1.0f));
 		square.verts.push_back(glm::vec3(0.0f /scalingFactor, -0.9f /scalingFactor, 1.0f));
 		square.verts.push_back(glm::vec3(0.9f /scalingFactor, 0.0f /scalingFactor, 1.0f));
-
+		//Adjust scaling factor to fit nested shapes
 		scalingFactor *= 2;
 	}
-
+	//Color the lines
 	for (int i = 0; i < iteration; i++) {
 		for (int j = 0; j < 8; j++) {
 			square.colors.push_back(glm::vec3(shadeFactor, 0.0f, 0.0f));
@@ -74,52 +64,42 @@ void Scene::displaySquaresAndDiamondsScene(int iteration) {
 		for (int k = 0; k < 8; k++) {
 			square.colors.push_back(glm::vec3(0.0f, shadeFactor, 0.0f));
 		}
+		//Adjust shade factore to create gradient
 		shadeFactor -= 0.2;
 	}
-
+	//Set draw mode
 	square.drawMode = GL_LINES;
-
-	//Construct vao and vbos for the triangle
+	//Construct vao and vbos 
 	RenderingEngine::assignBuffers(square);
-
-	//Send the triangle data to the GPU
-	//Must be done every time the triangle is modified in any way, ex. verts, colors, normals, uvs, etc.
+	//Send square data to the GPU
 	RenderingEngine::setBufferData(square);
-
-	//Add the triangle to the scene objects
+	//Add sqaure to scene objects
 	objects.push_back(square);
 }
 
 void Scene::displayParametricSprialScene(int iteration){
 	objects.clear();
-
-	//Our circle object
 	Geometry circle;
-
 	//RGB color parameters
 	double r = 1;
 	double g = 0;
 	double b = 0;
-
 	int numberOfLineSegments = 100;
 	float du = 2 * 3.14f / numberOfLineSegments;
-	for (float u = 0; u < iteration * 2 * 3.14f; u += du)
-	{
-		double xCoord = 0.038 * u * cos(u); //1 is important, change to something else for spiral
+	//Create spirals
+	for (float u = 0; u < iteration * 2 * 3.14f; u += du) {
+		double xCoord = 0.038 * u * cos(u); 
 		double yCoord = 0.038 * u * sin(u);
 		double zCoord = 1.0f;
-
 		circle.verts.push_back(glm::vec3(xCoord, yCoord, zCoord));
 		circle.colors.push_back(glm::vec3(r, g, b));
-	
 		//Creates a color gradient from red to green
 		r -= 0.0015;
 		g += 0.0015;
 	}
-	//Renders the line connecting any 2 points drawn
+	//Set draw mode
 	circle.drawMode = GL_LINE_STRIP;	
-	
-	//Construct VAO and VBOs for the circle
+	//Construct VAO and VBOs 
 	RenderingEngine::assignBuffers(circle);
 	RenderingEngine::setBufferData(circle);
 	objects.push_back(circle);
